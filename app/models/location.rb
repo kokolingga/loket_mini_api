0e44286428f1444a1ee5157a5867d76a7c4b1cc4
Koko Lingga <koko.lingga@go-jek.com>
2019-09-12 22:38:59 +0700

    [Location] add model's presence validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  # Validations
+  validates_presence_of :name, :address, :city, :google_map_url
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure each columns are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:google_map_url) }
 end
